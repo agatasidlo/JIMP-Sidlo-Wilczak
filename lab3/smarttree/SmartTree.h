48aa1b7e3a2ea41c64ea1d30c9ce3391c4d96f79
katarzynawilczak <kasia_9716@wp.pl>
2017-04-07 21:21:03 +0200

    UpdateSmartTree_4

--- lab3/smarttree/SmartTree.h
+++ lab3/smarttree/SmartTree.h
@@ -7,25 +7,21 @@ namespace datastructures {
         int value;
         std::unique_ptr<SmartTree> right;
         std::unique_ptr<SmartTree> left;
-
         SmartTree(int value) : value(value) {};
     };
+    std::unique_ptr <SmartTree> CreateLeaf(int value);
 
-    std::unique_ptr<SmartTree> CreateLeaf(int value);
+    std::unique_ptr <SmartTree>
+    InsertLeftChild(std::unique_ptr <SmartTree> tree, std::unique_ptr <SmartTree> left_subtree);
 
-    std::unique_ptr<SmartTree>
-    InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree);
+    std::unique_ptr <SmartTree>
+    InsertRightChild(std::unique_ptr <SmartTree> tree, std::unique_ptr <SmartTree> right_subtree);
 
-    std::unique_ptr<SmartTree>
-    InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree);
+    void PrintTreeInOrder(const std::unique_ptr <SmartTree> &unique_ptr, std::ostream *out);
 
-    void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out);
+    std::string DumpTree(const std::unique_ptr <SmartTree> &tree);
 
-    std::string DumpTree(const std::unique_ptr<SmartTree> &tree);
-
-    std::unique_ptr<SmartTree> RestoreTree1(const std::string &tree, int *i);
-
-    std::unique_ptr<SmartTree> RestoreTree(const std::string &tree);
+    std::unique_ptr <SmartTree> RestoreTree(const std::string &tree);
 }
 
 #endif //JIMP_EXERCISES_SMARTTREE_H
